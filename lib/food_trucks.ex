e17ab732f00bfd045e426a7d62bbdbf81a71804d
Stephanie Wood <wood.steph@gmail.com>
2023-05-04 14:17:23 -0700

    add truck filtering for food item

--- lib/food_trucks.ex
+++ lib/food_trucks.ex
@@ -2,16 +2,23 @@ defmodule FoodTrucks do
   # import data file
   @truck_data "./assets/truck_data.csv"
 
+  # get data for trucks that are approved. Assumption that we don't need to know about trucks that don't have a permit.
   def get_truck_data() do
-    File.stream!(@truck_data)
-    |> Stream.map(&String.split(&1, ","))
-    |> Stream.filter(fn
+    File.read!(@truck_data)
+    |> String.split("\n")
+    |> Enum.map(&String.split(&1, ","))
+    |> Enum.filter(fn
         ["locationid" | _] -> false
+        [_, _, _, _, _, _, _, _, _, _, "REQUESTED" | _] -> false
+        [_, _, _, _, _, _, _, _, _, _, "EXPIRED" | _] -> false
+        [_, _, _, _, _, _, _, _, _, _, "SUSPEND" | _] -> false
+        [_, _, _, _, _, _, _, _, _, _, "ISSUED" | _] -> false
         _ -> true
     end)
   end
 
-  def get_trucks() do
-    "tacos, burritos, ice cream"
+  def get_trucks(input) do
+    get_truck_data()
+    |> Enum.filter(fn [_, _, _, _, _, _, _, _, _, _, _, f | _] -> String.contains?(String.downcase(f), String.downcase(input)) end)
   end
 end
--- lib/food_trucks_web/live/trucks_live.ex
+++ lib/food_trucks_web/live/trucks_live.ex
@@ -2,7 +2,7 @@ defmodule FoodTrucksWeb.TrucksLive do
   use Phoenix.LiveView
 
   def mount(_params, _session, socket) do
-    trucks = FoodTrucks.get_trucks()
+    trucks = FoodTrucks.get_truck_data()
     {:ok, assign(socket, :trucks, trucks)}
   end
 
